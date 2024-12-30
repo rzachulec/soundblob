cf47194e96d7bc843796dcafca21141e68826de7
Jan Narozny <rzachulec@gmail.com>
2024-12-30 12:01:21 +0100

    Initial Commit

---
+++ soundblob/soundblobView.h
@@ -0,0 +1,12 @@
+//
+//  soundblobView.h
+//  soundblob
+//
+//  Created by jan on 30/12/2024.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface soundblobView : ScreenSaverView
+
+@end
---
+++ soundblob/soundblobView.m
@@ -0,0 +1,51 @@
+//
+//  soundblobView.m
+//  soundblob
+//
+//  Created by jan on 30/12/2024.
+//
+
+#import "soundblobView.h"
+
+@implementation soundblobView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
